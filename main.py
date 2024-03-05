bef96931690370ca6d74553792b3fe73f427f1fd
golmonew <162141528+golmonew@users.noreply.github.com>
2024-03-05 14:19:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,59 +26,54 @@ class Forwarder:
         print(f"{Fore.GREEN}Configuration set up successfully.{Fore.RESET}")
 
     def start(self):
-    os.system("cls")
-    if not os.path.exists("config.data"):
-        print(f"{Fore.RED}Configuration does not exist. Set up your account first!{Fore.RESET}")
-        return
+        os.system("cls")
+        if not os.path.exists("config.data"):
+            print(f"{Fore.RED}Configuration does not exist. Set up your account first!{Fore.RESET}")
+            return
 
-    file = configparser.RawConfigParser()
-    file.read('config.data')
-    api_id = file['cred']['id']
-    api_hash = file['cred']['hash']
-    phone = file['cred']['phone']
-    client = TelegramClient(phone, api_id, api_hash)
-    client.start()
+        file = configparser.RawConfigParser()
+        file.read('config.data')
+        api_id = file['cred']['id']
+        api_hash = file['cred']['hash']
+        phone = file['cred']['phone']
+        client = TelegramClient(phone, api_id, api_hash)
+        client.start()
 
-    # Provided Channel ID and Message ID
-    ch1 = -1002064776173  # Replace -1002064776173 with your channel ID
-    id1 = 46  # Replace 46 with your message ID
+        ch1 = int(input("Channel ID: "))
+        id1 = int(input("Message ID: "))
 
-    async def get_message():
-        message = await client.get_messages(ch1, ids=id1)
-        return message
+        async def get_message():
+            message = await client.get_messages(ch1, ids=id1)
+            return message
 
-    async def get_chat_id(client, group_link):  # Pass the client instance as an argument
-        entity = await client.get_entity(group_link)
-        chat_id = entity.id
-        return chat_id
+        async def get_chat_id(group_link):
+            entity = await client.get_entity(group_link)
+            chat_id = entity.id
+            return chat_id
 
-    with open("channels.txt", "r") as f:
-        lines = f.readlines()
-    with open("channels_ids.txt", "w") as w:
-        for i in lines:
-            chat_id = client.loop.run_until_complete(get_chat_id(client, i.strip()))  # Pass client to get_chat_id
-            print(chat_id)
-            w.write(str(chat_id) + "\n")
+        with open("channels.txt", "r") as f:
+            lines = f.readlines()
+            with open("channels_ids.txt", "w") as w:
+                for i in lines:
+                    chat_id = client.loop.run_until_complete(get_chat_id(i.strip()))
+                    print(chat_id)
+                    w.write(str(chat_id) + "\n")
 
-    async def forward_message():
-        while True:
-            with open("channels_ids.txt", "r") as y:
-                lines = y.readlines()
-                for id_chat in lines:
-                    chat_id = int(id_chat.strip())
-                    message = await get_message()
-                    try:
-                        await client.forward_messages(chat_id, message)
-                    except Exception as e:
-                        print(f"Error in chat {chat_id}. Message: {e}")
-                time.sleep(30)
+        async def forward_message():
+            while True:
+                with open("channels_ids.txt", "r") as y:
+                    lines = y.readlines()
+                    for id_chat in lines:
+                        chat_id = int(id_chat.strip())
+                        message = await get_message()
+                        try:
+                            await client.forward_messages(chat_id, message)
+                        except Exception as e:
+                            print(f"Error in chat {chat_id}. Message: {e}")
+                    time.sleep(30)
 
-    with client:
-        client.loop.run_until_complete(forward_message())
-
-
-
-
+        with client:
+            client.loop.run_until_complete(forward_message())
 
     def join_group(self):
         os.system("cls")
